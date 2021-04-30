99b6700288c33173d12ab42f972f3454a0815301
Ivan <tornadod@mail.ru>
2021-04-30 23:01:10 +0300

    Ready quest 3 lesson 2

---
+++ lesson2.3/lesson2.3/Program.cs
@@ -0,0 +1,23 @@
+﻿//	Определить, является ли введённое пользователем число чётным.
+using System;
+
+namespace lesson2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите целое число:");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            if ((Number%2) == 0)
+            {
+                Console.WriteLine($"Число {Number} является чётным.");
+            }
+            else
+            {
+                Console.WriteLine($"Число {Number} является нечётным.");
+            }
+        }
+    }
+}
